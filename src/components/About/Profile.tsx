8c088394f1a40579186d3c878d05285c1c2a640a
Francis <francis@al.co.za>
2025-01-04 21:38:38 -0600

    changed info

--- src/components/About/Profile.tsx
+++ src/components/About/Profile.tsx
@@ -4,10 +4,10 @@ const Profile = () => {
   return (
     <div className="space-y-6 group">
       <p className="text-lg text-gray-300 transition-colors duration-300">
-      I’m Francis Tumba, a passionate Data Engineer now based in the United States, currently pursuing a Master’s degree
-      in Computer Science at the University of Wisconsin-Green Bay. With extensive experience in data engineering, cloud technologies,
-      and software development, my expertise lies in transforming complex data into actionable insights. I have a keen interest in AI/ML 
-      and Data Science. Outside of tech, I’m a gamer and sports fanatic who loves exploring new challenges and crafting innovative solutions that make an impact
+      I’m Francis Tumba, a passionate Data Engineer now based in the United States, currently pursuing a Master’s degree in Computer Science at the 
+      University of Wisconsin-Green Bay. With extensive experience in data engineering, cloud technologies,and software development, my expertise lies
+      in transforming complex data into actionable insights. I have a keen interest in AI/ML and Data Science. Outside of tech, I’m a gamer and sports 
+      fanatic who loves exploring new challenges and crafting innovative solutions that make an impact
       </p>
       <div className="relative w-full h-[300px] rounded-lg overflow-hidden transform transition-all duration-300">
         <img
